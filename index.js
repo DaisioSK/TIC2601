9d1678990fa241640936cc7e27b8a91aeecda13d
DaisioSK <37562097+DaisioSK@users.noreply.github.com>
2021-09-26 08:39:43 +0800

    project init

---
+++ index.js
@@ -0,0 +1,26 @@
+//import
+const path = require('path');
+const express = require('express');
+
+//20210926 seakon: create the app instance
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/**
+ * path navi
+ * to be filled in later
+ */
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+//server
+var server = app.listen(80, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+   
+   console.log("Example app listening at http://%s:%s", host, port);
+});
+
